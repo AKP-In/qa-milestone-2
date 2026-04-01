1a5a84e17768c2dc5b67f4ea5a7ff22118b8b980
devop <devop@relevantaudience.com>
2026-04-01 18:01:15 +0700

    Added new function

--- parser.js
+++ parser.js
@@ -14,6 +14,12 @@ async function parseXML(xml) {
     return json;
 }
 
+async function getUrls(xml) {
+    const json = await parseXML(xml);
+    const urls = json.urlset.url.map((url) => url.loc);
+    return urls;
+}
+
 // This is the fetch XML function
 fetchXML('https://www.google.com/sitemap.xml').then((xml) => {
     const jsonObj = parser.parse(xml);
