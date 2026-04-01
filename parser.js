5ad35de72ad8b98bed6ae133756c4d7e6fa6afaa
devop <devop@relevantaudience.com>
2026-04-01 17:51:43 +0700

    Removed unused function. Added comment. Changed fetch URL

--- parser.js
+++ parser.js
@@ -7,15 +7,9 @@ async function fetchXML(xml) {
     return data;
 }
 
-async function parseXML(xml) {
-    const parser = new XMLParser();
-    const json = parser.parse(xml);
-    return json;
-}
 
-
-
-fetchXML('https://www.relevantaudience.com/sitemap.xml').then((xml) => {
+// This is the fetch XML function
+fetchXML('https://www.google.com/sitemap.xml').then((xml) => {
     const jsonObj = parser.parse(xml);
     console.log(JSON.stringify(jsonObj, null, 2));
     const urls = jsonObj.urlset.url.map((url) => url.loc);
